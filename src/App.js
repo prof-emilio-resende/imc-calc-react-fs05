046ab32bb54bb904fdad5b522242340e8af99523
emilio.resende <emilio.resende@faculdadeimpacta.com.br>
2023-03-29 20:14:44 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import ImcView from "./views/ImcView";
+import ImcController from "./controllers/ImcController";
+import Person from "./domain/Person";
+import { useState } from "react";
+
 
 function App() {
+
+  const [person, setPerson] = useState(new Person())
+
+  const calculateImc = async () => {
+    const heightElem = document.querySelector("#altura");
+    const weightElem = document.querySelector("#peso");
+  
+    if (!heightElem) throw Error("height is required!");
+    if (!weightElem) throw Error("weight is required!");
+  
+    const height = Number(heightElem.value);
+    const weight = Number(weightElem.value);
+  
+    const p = new Person(height, weight);
+  
+    const ctrl = new ImcController();
+    const result = await ctrl.calculate(p.toObject());
+  
+    console.log(result);
+    setPerson(result);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="data">
+        <div className="form">
+          <div className="row">
+          </div>
+          <hr />
+          <div className="row">
+            <label>Altura</label>
+            <input id="altura" placeholder="0.00" />
+          </div>
+          <div className="row">
+            <label>Peso</label>
+            <input id="peso" placeholder="0.00" />
+          </div>
+          <button type="button" onClick={calculateImc} className="action">
+            Calcular
+          </button>
+        </div>
+      </div>
+      <hr />
+      <div>
+        <ImcView className="data" person={person} />
+      </div>
     </div>
   );
 }
